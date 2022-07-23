3837352704e9b73f8993a98ecf756e79d19adc04
root <rajeshkatari983@gmail.com>
2022-07-23 11:02:31 +0000

    banner

--- src/components/RestaurantDetails/index.js
+++ src/components/RestaurantDetails/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
 import Loader from 'react-loader-spinner'
 import './index.css'
 import RestaurantBanner from '../RestaurantBanner'
@@ -6,20 +7,82 @@ import Header from '../Header'
 import Footer from '../Footer'
 
 class RestaurantDetails extends Component {
-  state = {isLoading: false, restDetails: {}}
+  state = {isLoading: false, restDetails: {}, foodItemsList: []}
+
+  componentDidMount() {
+    this.getRestaurantDetails()
+  }
+
+  getRestaurantDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    this.setState({isLoading: true})
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const apiUrl = `https://apis.ccbp.in/restaurants-list/${id}`
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+
+    const data = await fetch(apiUrl, options)
+
+    const updatedData = await data.json()
+
+    const updatedRestDetails = {
+      id: updatedData.id,
+      imageUrl: updatedData.image_url,
+      name: updatedData.name,
+      location: updatedData.location,
+      rating: updatedData.rating,
+      reviewsCount: updatedData.reviews_count,
+      costForTwo: updatedData.cost_for_two,
+      cuisine: updatedData.cuisine,
+    }
+
+    const foodItems = updatedData.food_items.map(eachItem => ({
+      cost: eachItem.cost,
+      id: eachItem.id,
+      imageUrl: eachItem.image_url,
+      name: eachItem.name,
+      rating: eachItem.rating,
+    }))
+
+    this.setState({
+      isLoading: false,
+      restDetails: updatedRestDetails,
+      foodItemsList: foodItems,
+    })
+  }
 
   renderLoader = () => (
-    <div testid="restaurant-details-loader" className="restaurant-loader">
+    <div
+      testid="restaurant-details-loader"
+      className="restaurant-details-loader"
+    >
       <Loader type="ThreeDots" color="#F7931E" height={50} width={50} />
     </div>
   )
 
+  renderDetails = () => {
+    const {restDetails} = this.state
+
+    return <RestaurantBanner restDetails={restDetails} />
+  }
+
   render() {
+    const {isLoading} = this.state
     return (
       <>
         <Header />
         <div className="restaurant-details-bg-container">
-          Restaurant Details
+          {isLoading ? this.renderLoader() : this.renderDetails()}
         </div>
         <Footer />
       </>
